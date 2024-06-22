4716579d877658aebfe077ab382195a5d8d7cb1c
Jeremy <j.salvatierragonzales10@gmail.com>
2024-06-22 18:00:33 -0500

    E agregado pagina reglas

---
+++ src/app/reglas/page.tsx
@@ -0,0 +1,15 @@
+const Reglas = () => {
+  return (
+    <div className="regla_contenedor">
+      <p className="regla_titulo">Reglas</p>
+      <p>Tres en raya es un juego bastante antiguo, para poder jugar esto</p>
+      hay unas reglas las cuales son:
+      <p> 1.Consigue un lapiz y papel</p>
+      <p>2.Debenexistir 2 jugadores </p>
+      <p>3. Se utliza la x y o</p>
+      <p>4.Deben completar de manera vertical, horizontal y o por el medio</p>
+      <p>5. una vez q un jugador complete los 3 juntos gana.</p>
+    </div>
+  );
+};
+export default Reglas;
